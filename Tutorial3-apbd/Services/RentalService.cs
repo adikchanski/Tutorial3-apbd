24f9b7c8fbc274fa9f5d35aef97ec6832ca7b91a
adikchanski <adik.sabirov.zso@gmail.com>
2026-03-23 19:44:47 +0100

    ReturnEquipment and RentEquipment functions implementation

--- Tutorial3-apbd/Services/RentalService.cs
+++ Tutorial3-apbd/Services/RentalService.cs
@@ -10,5 +10,41 @@ public class RentalService
     {
         if (!equipment.IsAvailable || !equipment.IsOperational)
             throw new Exception("Unavailable equipment");
+
+        int activeRentals = rentals.Count(r => r.User == user && !r.IsReturned);
+        
+        if (activeRentals >= user.RentalLimit)
+            throw new Exception("Rental limit exceeded");
+        
+        rentals.Add(new Rental
+        {
+            User = user,
+            Equipment = equipment,
+            RentDate = DateTime.Now,
+            DueDate = DateTime.Now.AddDays(days),
+        });
+        
+        equipment.IsAvailable = false;
+    }
+
+    public void ReturnEquipment(Equipment equipment)
+    {
+        Rental? foundRental = null;
+        foreach (var rental in rentals)
+        {
+            if (rental.Equipment == equipment && !rental.IsReturned)
+            {
+                foundRental = rental;
+                break;
+            }
+        }
+        if (foundRental == null)
+            throw new Exception("Rental not found");
+
+        foundRental.ReturnDate = DateTime.Now;
+        equipment.IsAvailable = true;
+        
+        decimal penalty = foundRental.CalculatePenalty();
+        Console.WriteLine($"Penalty: {penalty}");
     }
 }
